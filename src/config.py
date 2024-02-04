87fdb5e867d7656a344a82a22b927c120984133c
AlexeyKurlevsky <kurlevsky@gmail.com>
2024-02-04 23:03:56 +0300

    initial commit

--- src/config.py
+++ src/config.py
@@ -1,8 +1,12 @@
 import os
+import logging
 
 from dotenv import load_dotenv
 
 load_dotenv()
+
+FORMAT = '%(asctime)s %(message)s'
+logging.basicConfig(level=logging.INFO, format=FORMAT)
 
 HUNTFLOW_ACCESS_TOKEN = os.getenv('HUNTFLOW_ACCESS_TOKEN')
 HUNTFLOW_REFRESH_TOKEN = os.getenv('HUNTFLOW_REFRESH_TOKEN')
--- src/func.py
+++ src/func.py
@@ -1,7 +1,23 @@
+import logging
+import json
+
 from huntflow_api_client import HuntflowAPI
 from huntflow_api_client.tokens import ApiToken
 
-from config import HUNTFLOW_ACCESS_TOKEN, HUNTFLOW_REFRESH_TOKEN
+from src.config import HUNTFLOW_ACCESS_TOKEN, HUNTFLOW_REFRESH_TOKEN
 
-hunt_token = ApiToken(access_token=HUNTFLOW_ACCESS_TOKEN, refresh_token=HUNTFLOW_REFRESH_TOKEN)
-hunt_client = HuntflowAPI(token=hunt_token, auto_refresh_tokens=True)
+hunt_token = ApiToken(access_token=HUNTFLOW_ACCESS_TOKEN,
+                      refresh_token=HUNTFLOW_REFRESH_TOKEN)
+hunt_client = HuntflowAPI(base_url='https://api.huntflow.ru',
+                          token=hunt_token,
+                          auto_refresh_tokens=True)
+
+
+async def get_me():
+    resp = await hunt_client.request(method='GET', path='me')
+    if resp.status_code != 200:
+        logging.info('Status code from getMe request %s' % resp.status_code)
+        raise ValueError(resp.text)
+    logging.info('Status code from getMe request %s' % resp.status_code)
+    res = json.loads(resp.text)
+    return res
