ab3ed36ee53ee88b075caec576842e7f2269fcba
AlexeyKurlevsky <kurlevsky@gmail.com>
2024-02-06 00:27:30 +0300

    add new vacancies

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ import os
 import logging
 
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
 load_dotenv()
 
@@ -10,4 +11,20 @@ logging.basicConfig(level=logging.INFO, format=FORMAT)
 
 HUNTFLOW_ACCESS_TOKEN = os.getenv('HUNTFLOW_ACCESS_TOKEN')
 HUNTFLOW_REFRESH_TOKEN = os.getenv('HUNTFLOW_REFRESH_TOKEN')
+
+DATABASE_HOST = os.getenv('DATABASE_HOST')
+DATABASE_PORT = os.getenv('DATABASE_PORT')
+DATABASE_HUNTFLOW = os.getenv('DATABASE_HUNTFLOW')
+DATABASE_HUNTFLOW_USERNAME = os.getenv('DATABASE_HUNTFLOW_USERNAME')
+DATABASE_HUNTFLOW_PASSWORD = os.getenv('DATABASE_HUNTFLOW_PASSWORD')
+
+SQLALCHEMY_DB_URI = (f'postgresql+psycopg2://'
+                     f'{DATABASE_HUNTFLOW_USERNAME}:{DATABASE_HUNTFLOW_PASSWORD}@'
+                     f'{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_HUNTFLOW}')
+
+MAX_VACANCIES_ON_PAGE = 50
+
+engine = create_engine(SQLALCHEMY_DB_URI)
+
+
 
