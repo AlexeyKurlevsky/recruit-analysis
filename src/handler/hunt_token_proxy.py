7460214df1a944f18125180c3952c1d6bd010848
AlexeyKurlevsky <kurlevsky@gmail.com>
2024-02-25 13:44:12 +0300

    add postgres container

--- src/handler/hunt_token_proxy.py
+++ src/handler/hunt_token_proxy.py
@@ -21,6 +21,13 @@ class HuntTokenProxy(DummyHuntflowTokenProxy):
     async def update(self, refresh_result: Dict[str, Any]) -> None:
         self._token = convert_refresh_result_to_hf_token(refresh_result, self._token)
         os.environ["HUNTFLOW_ACCESS_TOKEN"] = self._token.access_token
-        set_env_in_file("HUNTFLOW_ACCESS_TOKEN")
+        logging.info('Set new access token huntflow')
         os.environ["HUNTFLOW_REFRESH_TOKEN"] = self._token.refresh_token
-        set_env_in_file("HUNTFLOW_REFRESH_TOKEN")
+        logging.info('Set new refresh token huntflow')
+        try:
+            set_env_in_file("HUNTFLOW_ACCESS_TOKEN")
+            logging.info('Rewrite new huntflow access token in .env file')
+            set_env_in_file("HUNTFLOW_REFRESH_TOKEN")
+            logging.info('Rewrite new huntflow refresh token in .env file')
+        except Exception as ex:
+            logging.error(ex)
