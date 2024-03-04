5e0477cf28d4ef5e6d0fd7289b62c9bb5f91e941
AlexeyKurlevsky <kurlevsky@gmail.com>
2024-03-05 00:32:14 +0300

    add token variables

--- src/config.py
+++ src/config.py
@@ -2,17 +2,27 @@ import os
 import logging
 import json
 
+from airflow import AirflowException
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from airflow.settings import DAGS_FOLDER
+from airflow.models import Variable
 
 load_dotenv()
 
 FORMAT = "%(asctime)s %(message)s"
 logging.basicConfig(level=logging.INFO, format=FORMAT)
 
-HUNTFLOW_ACCESS_TOKEN = os.getenv("HUNTFLOW_ACCESS_TOKEN")
-HUNTFLOW_REFRESH_TOKEN = os.getenv("HUNTFLOW_REFRESH_TOKEN")
+# Для разработки
+# HUNTFLOW_ACCESS_TOKEN = os.getenv("HUNTFLOW_ACCESS_TOKEN")
+# HUNTFLOW_REFRESH_TOKEN = os.getenv("HUNTFLOW_REFRESH_TOKEN")
+
+HUNTFLOW_ACCESS_TOKEN = Variable.get("HUNTFLOW_ACCESS_TOKEN")
+HUNTFLOW_REFRESH_TOKEN = Variable.get("HUNTFLOW_REFRESH_TOKEN")
+
+if HUNTFLOW_ACCESS_TOKEN is None or HUNTFLOW_REFRESH_TOKEN is None:
+    raise AirflowException('Set HUNTFLOW_ACCESS_TOKEN or HUNTFLOW_REFRESH_TOKEN')
+
 HUNTFLOW_USERNAME = os.getenv("HUNTFLOW_USERNAME")
 HUNTFLOW_PASSWORD = os.getenv("HUNTFLOW_PASSWORD")
 
--- src/handler/hunt_token_proxy.py
+++ src/handler/hunt_token_proxy.py
@@ -8,12 +8,22 @@ from huntflow_api_client.tokens.proxy import (
     DummyHuntflowTokenProxy,
     convert_refresh_result_to_hf_token,
 )
+from airflow.models import Variable
 
 
-def set_env_in_file(key: str) -> None:
+def set_env_in_file(key: str, value: str) -> None:
+    """
+    Обновить токены в переменных окружениях
+    и переменных airflow
+    :param key:
+    :param value:
+    :return:
+    """
+    os.environ[key] = value
     dotenv_file = dotenv.find_dotenv()
     dotenv.load_dotenv(dotenv_file)
     dotenv.set_key(dotenv_file, key, os.environ[key])
+    Variable.update(key=key, value=value)
     logging.info("Set new variable %s" % key)
 
 
@@ -23,14 +33,10 @@ class HuntTokenProxy(DummyHuntflowTokenProxy):
 
     async def update(self, refresh_result: Dict[str, Any]) -> None:
         self._token = convert_refresh_result_to_hf_token(refresh_result, self._token)
-        os.environ["HUNTFLOW_ACCESS_TOKEN"] = self._token.access_token
-        logging.info("Set new access token huntflow")
-        os.environ["HUNTFLOW_REFRESH_TOKEN"] = self._token.refresh_token
-        logging.info("Set new refresh token huntflow")
         try:
-            set_env_in_file("HUNTFLOW_ACCESS_TOKEN")
+            set_env_in_file("HUNTFLOW_ACCESS_TOKEN", self._token.access_token)
             logging.info("Rewrite new huntflow access token in .env file")
-            set_env_in_file("HUNTFLOW_REFRESH_TOKEN")
+            set_env_in_file("HUNTFLOW_REFRESH_TOKEN", self._token.refresh_token)
             logging.info("Rewrite new huntflow refresh token in .env file")
         except Exception as ex:
             logging.error(ex)
