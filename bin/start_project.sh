00cb2ec95dc8c41e8eb4186171e2dc80aba1a854
AlexeyKurlevsky <kurlevsky@gmail.com>
2024-03-05 21:47:02 +0300

    add env

--- bin/start_project.sh
+++ bin/start_project.sh
@@ -5,4 +5,4 @@ echo "AIRFLOW_UID=$(id -u)" >> .env
 
 docker compose -f ./postgres-compose.yml up -d
 docker compose -f ./airflow-compose.yml up -d --build
-#docker compose -f ./superset-compose.yml up -d
+docker compose -f ./superset-compose.yml up -d
