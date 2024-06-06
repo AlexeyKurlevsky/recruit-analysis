d45f657d08df3b6a8d080bc4a25e2127219e00d8
AlexeyKurlevsky <kurlevsky@gmail.com>
2024-06-06 20:43:36 +0300

    add nginx conf

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -41,6 +41,8 @@ EXAMPLES_HOST = os.getenv("EXAMPLES_HOST")
 EXAMPLES_PORT = os.getenv("EXAMPLES_PORT")
 EXAMPLES_DB = os.getenv("EXAMPLES_DB")
 
+ENABLE_PROXY_FIX = True
+
 # The SQLAlchemy connection string.
 SQLALCHEMY_DATABASE_URI = (
     f"{DATABASE_DIALECT}://"
