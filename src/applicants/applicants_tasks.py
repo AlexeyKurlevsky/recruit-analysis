e1026f6ff1c7715e35964145fd63b95a3b61414f
AlexeyKurlevsky <kurlevsky@gmail.com>
2024-07-08 23:38:03 +0300

    changed logic

--- src/applicants/applicants_tasks.py
+++ src/applicants/applicants_tasks.py
@@ -2,12 +2,8 @@ import logging
 
 from airflow.models import TaskInstance
 
-from src.applicants.func import (
-    check_status_applicants,
-    insert_info_applicant_on_vacancies,
-    update_info_applicant_on_vacancies,
-)
-from src.db.queries import check_vac_in_statistic, get_vacancy_besides_state
+from src.applicants.utils import handle_vacancy_with_status
+from src.db.queries import get_vacancy_besides_state
 from src.parser.hunt_parser import HuntFlowParser
 
 
@@ -25,22 +21,7 @@ def update_statistic_vacancies(ti: TaskInstance, **kwargs) -> None:
             if applicant_info is None:
                 logger.error(f"unable to obtain information about candidates for the vacancy {vacancy.id}")
                 continue
-            for status_id, name_and_value in applicant_info.items():
-                check_status_applicants(status_id, name_and_value[0])
-                logger.info(f"Проверяю запись для вакансии {vacancy.id} по статусу {status_id}")
-                vacancy_stat_info = check_vac_in_statistic(vacancy.id, status_id)
-                if vacancy_stat_info:
-                    logger.info(
-                        f"Обновляю запись {vacancy_stat_info[0].id} для вакансии {vacancy.id} со статусом {status_id}"
-                    )
-                    update_info_applicant_on_vacancies(
-                        db_id=vacancy_stat_info[0].id, status_id=int(status_id), value=name_and_value[1]
-                    )
-                else:
-                    logger.info(f"Добавляю новую запись для вакансии {vacancy.id} со статусом {status_id}")
-                    insert_info_applicant_on_vacancies(
-                        vac_id=vacancy.id, status_id=int(status_id), value=name_and_value[1]
-                    )
+            handle_vacancy_with_status(applicant_info=applicant_info, vacancy=vacancy)
         parse.logout()
     except Exception as ex:
         logger.error(ex)
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,7 @@
 import logging
-from src.applicants.func import check_status_applicants, insert_info_applicant_on_vacancies, update_info_applicant_on_vacancies
-from src.db.queries import check_vac_in_statistic, get_all_new_vacancies
+from src.db.tables import AllVacancies
+from src.applicants.utils import handle_vacancy_with_status
+from src.db.queries import get_last_vacancy_statistic_with_status, get_all_new_vacancies
 from src.parser.hunt_parser import HuntFlowParser
 from requests import session
 
@@ -18,16 +19,11 @@ from src.vacancies.vacancy_tasks import get_new_vacancies
 
 def test_login():
     parser = HuntFlowParser()
-    statistic = parser.get_vacancy_stat_info(3700192)
-    for status_id, name_and_value in statistic.items():
-        check_status_applicants(status_id, name_and_value[0])
-        vacancy_stat_info = check_vac_in_statistic(3700192, status_id)
-        if vacancy_stat_info:
-            update_info_applicant_on_vacancies(
-                db_id=vacancy_stat_info[0].id, status_id=int(status_id), value=name_and_value[1]
-            )
-        else:
-            insert_info_applicant_on_vacancies(
-                vac_id=3700192, status_id=int(status_id), value=name_and_value[1]
-            )
+    vacancy = AllVacancies()
+    vacancy.id = 3700192
+    vacancy.position = "test_position"
+    statistic = parser.get_vacancy_stat_info(vacancy.id)
+    if statistic is None:
+        raise ValueError("Не смог получить информацию по статистике вакансии")
+    handle_vacancy_with_status(applicant_info=statistic, vacancy=vacancy)
     parser.logout()
